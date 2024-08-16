8f339a45c237031a2d76aae23426b3508e0e5ddf
Akash Saha <143631612+akash-saha-01@users.noreply.github.com>
2024-08-16 05:37:35 +0000

    Data Types

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node js not browser
+
+console.log(33 + 0)
+// code readablity
+console.log("akash")
+
+
+// data types
+let name = "akash" // String
+let age = 54 // int
+let isLoggedIn = false // Boolean
+let state;
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => stand alone value or type
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); // type => object
+console.log(typeof undefined); // type => undefined
