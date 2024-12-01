bf294689c3cbaf45df81aec85f0bd79a9084cca4
Akash Saha <143631612+akash-saha-01@users.noreply.github.com>
2024-12-01 05:13:12 +0000

    Conversion Of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+let score = undefined
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// Number Convertion
+//  33 => 33
+// "33abc" => NaN (Not a Number)
+// "null" => 0
+// undefined = NaN
+// string => NaN
+// true/false => 1/0
+
+let isLoggedIn = ""
+
+let booleanIsLogedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLogedIn);
+
+// Boolean Convertion
+
+// 1/0 => true/false
+// "" => false
+// "string" => true
+
+let someNum = 34
+
+let stringNumber = String(someNum)
+console.log(stringNumber);
+console.log(typeof stringNumber)
+
+
+
+
+
+
